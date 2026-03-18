0857f7d64fc178a0182c6b1b2d58321c162ba342
wtobar <sewerty98@gmail.com>
2026-03-18 10:25:39 -0500

    Cmmit de Respaldo

--- Controllers/ControladorFactura.cs
+++ Controllers/ControladorFactura.cs
@@ -12,7 +12,7 @@ using Sistema_de_Facturacion_Electronica.Modelos;
 namespace Sistema_de_Facturacion_Electronica.Controllers
 {
     [Controller]
-    [Route("api/factura")]
+    [Route("api/v1/facturas")]
     public class ControladorFactura : ControllerBase
     {
         private readonly IFactura _factura;
@@ -22,7 +22,7 @@ namespace Sistema_de_Facturacion_Electronica.Controllers
         private readonly ICliente _cliente;
         private readonly ICalculoFactura _calculoFactura;
 
-        public ControladorFactura(IFactura factura,/*IExportacionXML xml,IValidacionXSD xsd,*/UserManager<Usuario> userManager,ICliente cliente,ICalculoFactura calculoFactura) 
+        public ControladorFactura(IFactura factura,/*IExportacionXML xml,IValidacionXSD xsd,*/UserManager<Usuario> userManager, ICliente cliente, ICalculoFactura calculoFactura)
         {
             _factura = factura;
             /*_xml = xml;
@@ -33,8 +33,8 @@ namespace Sistema_de_Facturacion_Electronica.Controllers
         }
 
         [Authorize]
-        [HttpGet("obtenerfactura/{IdFactura:int}")]
-        public async Task<IActionResult> ObtenerFacturaID([FromRoute] int IdFactura) 
+        [HttpGet("buscar/{IdFactura:int}")]
+        public async Task<IActionResult> ObtenerFacturaID([FromRoute] int IdFactura)
         {
             var ModeloFactura = await _factura.ObtenerFacturaId(IdFactura);
             if (ModeloFactura == null) return NotFound($"La Factrua con el id {IdFactura} no se encuentra registrada en el sistema");
@@ -42,8 +42,8 @@ namespace Sistema_de_Facturacion_Electronica.Controllers
         }
 
         [Authorize]
-        [HttpGet("obtenerfacturas")]
-        public async Task<IActionResult> ObtenerFacturas([FromQuery] QueryFactura NodoQuery) 
+        [HttpGet("buscar")]
+        public async Task<IActionResult> ObtenerFacturas([FromQuery] QueryFactura NodoQuery)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
             var ModeloFactura = await _factura.ObtenerFacturas(NodoQuery);
@@ -55,42 +55,49 @@ namespace Sistema_de_Facturacion_Electronica.Controllers
         [HttpPost("registrarfactura")]
         public async Task<IActionResult> RegistrarFactura([FromBody] CrearFacturaDTO NodoFactura)
         {
-            if (!ModelState.IsValid) 
+            if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             var ModeloUsuario = User.GetGivenName();
             var AppUsuario = await _userManager.FindByNameAsync(ModeloUsuario);
             var ModeloCliente = await _cliente.ObtenerClienteNombre(NodoFactura.NombreCliente);
-            var ModeloFactura = await _factura.RegistrarFactura(NodoFactura.ToFactura(AppUsuario!.Id,ModeloCliente!.Id));
-            if (ModeloFactura == null) 
+            var ModeloFactura = await _factura.RegistrarFactura(NodoFactura.ToFactura(AppUsuario!.Id, ModeloCliente!.Id));
+            if (ModeloFactura == null)
             {
-                return StatusCode(500,"Error Interno del Servidor");
+                return StatusCode(500, "Error Interno del Servidor");
             }
 
-            return CreatedAtAction(nameof(ObtenerFacturaID), new {IdFactura = ModeloFactura.Id},ModeloFactura.ToFacturaDTO());
+            return CreatedAtAction(nameof(ObtenerFacturaID), new { IdFactura = ModeloFactura.Id }, ModeloFactura.ToFacturaDTO());
         }
 
         [Authorize]
         [HttpPost("generaritem")]
-        public async Task<IActionResult> GenerarItem([FromBody] CrearItemDTO NodoItem) 
+        public async Task<IActionResult> GenerarItem([FromBody] CrearItemDTO NodoItem)
         {
             if (!ModelState.IsValid) return BadRequest(ModelState);
             var FacturaModelo = await _factura.ObtenerFacturaId(NodoItem.IdFactura);
-            var ItemModelo = await _factura.CrearItems(NodoItem.ToItem(),FacturaModelo!);
-            //await _calculoFactura.Facturacion(FacturaModelo!);
-            if (ItemModelo == null) return StatusCode(500,"Error Interno del Servidor");
+            var ItemModelo = await _factura.CrearItems(NodoItem.ToItem(), FacturaModelo!);
+            if (ItemModelo == null) return StatusCode(500, "Error Interno del Servidor");
             return Ok(ItemModelo.ToItemDTO());
         }
 
         [Authorize]
         [HttpGet("{IdFactura:int}")]
-        public async Task<IActionResult> ValidarFactura([FromRoute] int IdFactura) 
+        public async Task<IActionResult> ValidarFactura([FromRoute] int IdFactura)
         {
-            //var ModeloFactura = await _factura.ObtenerFacturaId(IdFactura);
             var ModeloFactura = await _calculoFactura.Facturacion(IdFactura);
             if (ModeloFactura == null) return NotFound($"La Factura con el id {IdFactura} no se encuntra registrado");
             return Ok(ModeloFactura.ToFacturaDTO());
-        }       
+        }
+
+        [Authorize]
+        [HttpDelete("{IdFactura:int}")]
+        public async Task<IActionResult> EliminarFactura([FromRoute] int IdFactura)
+        {
+            var ModeloFactura = await _factura.AnularFactura(IdFactura);
+            if (ModeloFactura == null) return NotFound($"La Factura con el id {IdFactura} no se encuentra registrado en el sistema");
+            return NoContent();
+        }
     }
 }
